20188f3b583b4f7636376db64f95a6d216ca5c6e
Isaac Nygaard <isaac@nyrix.com>
2025-02-15 18:16:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Development setup
+
+> sudo apt install xfce4-dev-tools autopoint libxfce4ui-2-dev
+> ./autogen.sh
---
+++ hosts-plugin/hosts.h
@@ -0,0 +1,24 @@
+#ifndef __ISAAC_H__
+#define __ISAAC_H__
+
+G_BEGIN_DECLS
+
+/* plugin structure */
+typedef struct {
+	XfcePanelPlugin *plugin;
+
+	/* panel widgets */
+	GtkWidget       *ebox;
+	GtkWidget       *hvbox;
+	GtkWidget       *label;
+
+	/* sample settings */
+	gchar           *setting1;
+	gint             setting2;
+	gboolean         setting3;
+}
+SamplePlugin;
+
+void sample_save (XfcePanelPlugin *plugin, SamplePlugin *sample);
+
+G_END_DECLS
